4a9f6ae61dcff4e0e4e3b37193e57712b14ba2bf
Carly Huang <c85615@gmail.com>
2020-04-20 23:16:32 -0700

    Finish day 8 part 2

--- 8/8.js
+++ 8/8.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const Image = require('./Image')
 
 function readInput() {
-    return fs.readFileSync('./input.txt', 'utf8').split('')
+    return fs.readFileSync('./input.txt', 'utf8').split('').map(Number)
 }
 const imageData = readInput()
 
@@ -10,3 +10,4 @@ const image = new Image()
 image.build(imageData)
 const part1Ans = image.validate()
 console.log("Part 1 answer is " + part1Ans)
+image.decode()
--- 8/Image.js
+++ 8/Image.js
@@ -7,15 +7,16 @@ class Image {
     constructor() {
         this.layers = []
         this.layer0Map = []
+        this.finalImage = []
     }
 
     build(data) {
         let i = 0
         while (data.length > 0) {
-            let layerData = data.splice(0, 150)
+            let layerData = data.splice(0, IMG_SIZE)
             let layer = new Layer(layerData)
             this.layers.push(layer)
-            this.layer0Map[i] = layer.countDigit('0')
+            this.layer0Map[i] = layer.countDigit(0)
             i++
         }
     }
@@ -35,7 +36,44 @@ class Image {
     validate() {
         let layerIndex = this.getIndexOfMin0Layer()
         const targetLayer = this.layers[layerIndex]
-        return targetLayer.multiply('1', '2')
+        return targetLayer.multiply(1, 2)
+    }
+
+    print() {
+        let formatedImg = []
+        for (let y = 0; y < IMG_HEIGHT; y++) {
+            formatedImg[y] = this.finalImage.splice(0, IMG_WIDTH)
+            let currentPixelBar = ''
+            for (let pixel of formatedImg[y]) {
+                switch (pixel) {
+                    case 0:
+                        currentPixelBar += ' '
+                        break
+                    case 1:
+                        currentPixelBar += '#'
+                        break
+                    default:
+                        break
+                }
+            }
+            console.log(currentPixelBar)
+        }
+    }
+
+    decode() {
+        // 0 is black, 1 is white, and 2 is transparent
+        for (let i = 0; i < IMG_SIZE; i++) {
+            let colour = 2
+            for (let layer of this.layers) {
+                let current_colour = layer.data[i]
+                if (current_colour == 0 || current_colour == 1) {
+                    colour = current_colour
+                    break
+                }
+            }
+            this.finalImage[i] = colour
+        }
+        this.print()
     }
 
 }
